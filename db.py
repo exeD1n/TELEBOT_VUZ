1e51462251f12d5f380e04cd3bdd19c2e45d1042
Daniil Olegovich <daniillutaev1@gmail.com>
2022-10-06 17:23:04 +0400

    База данных

--- db.py
+++ db.py
@@ -1,1 +1,13 @@
 # Подключение к базе данных
+
+import pyodbc 
+
+connection_to_db = pyodbc.connect(r'Driver={SQL Server};Server=85.236.170.148,444;Database=PythonBotPguty;username=stud;password=stud')
+cursor = connection_to_db.cursor()
+cursor.execute('SELECT subject, age FROM Subject')
+while 1:
+    row = cursor.fetchone()
+    if not row:
+        break
+    print(row.subject)
+connection_to_db.close()
